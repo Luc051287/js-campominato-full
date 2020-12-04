b0aa56103bf6a802da743881d74c370bd248c973
Luca Gentili <l.gentili87@icloud.com>
2020-12-04 19:45:55 +0100

    Seventh Version

--- js/script.js
+++ js/script.js
@@ -1,105 +1,41 @@
 // ES6 aggiunge la classe, per cui si potrebbe scrivere box come classe e metterci dentro sia le proprietà che qualche funzione! Provare a pensare se può essere utilizzato VueJS!
-// Mettere protezione per gli elementi undefined! 
+// Mettere protezione per gli elementi undefined!
+// farlo funzionare con livelli di difficoltà più alti
 
 $(document).ready(function() {
 
-  const arraybombs =[];
-  const field = [];
+  // const box = {
+  //   position: [0,0],
+  //   bombs: 0,
+  //   isBomb: false,
+  //   id: 0,
+  //   opened: false
+  // };
 
-  const box = {
-    position: [0,0],
-    bombs: 0,
-    isBomb: false,
-    id: 0,
-    opened: false
-  };
+
+  class Box {
+   constructor(id, position, bombs, isBomb, isOpened) {
+      this.id = id;
+      this.position = position;
+      this.bombs = bombs;
+      this.isBomb = isBomb;
+      this.isOpened = isOpened;
+    }
+  }
+
+  const fieldBoxes = $("#box_field");
 
   let level = parseInt(prompt("Scegli il livello"));
 
-  console.log(levelChoise(level));
+  // Creo il campo
+  let newField = field(level);
 
-  let contatore = 0;
+  // genero le bombe
+  let newArrayBombs = arrayBombs(level, newField);
 
-  for (let x=1; x<=levelChoise(level)[1][0]; x++) {
-    for (let y=1; y<=levelChoise(level)[1][1]; y++) {
-      let newBox = Object.create(box);
-      newBox.position = [x,y];
-      newBox.id = contatore;
-      field.push(newBox);
-      contatore += 1;
-    }
-  }
+  generatefield (level, newField, newArrayBombs, fieldBoxes);
 
-  console.log(field);
-
-  while (arraybombs.length < levelChoise(level)[2]) {
-    bomb = levelChoise(level)[0];
-    if (checkIfDiff(field[bomb].position, arraybombs)) {
-      field[bomb].bombs = "B";
-      field[bomb].isBomb = true;
-      arraybombs.push(field[bomb]);
-    }
-  }
-
-  console.log(arraybombs);
-
-  let numOfBombs = 0;
-  let counter = 0;
-
-
-  // Provare a fare foreach con funzione
-  for (let x=1; x<=levelChoise(level)[1][0]; x++) {
-    for (let y=1; y<=levelChoise(level)[1][1]; y++) {
-      numOfBombs = 0;
-      if (!checkBombs(arraybombs, x, y)) {
-        if (checkBombs(arraybombs, x-1, y-1)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x-1, y)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x-1, y+1)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x, y-1)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x, y+1)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x+1, y-1)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x+1, y)) {
-          numOfBombs += 1
-        }
-        if (checkBombs(arraybombs, x+1, y+1)) {
-          numOfBombs += 1
-        }
-
-        if (!field[counter].isBomb) {
-          field[counter].bombs = numOfBombs;
-        }
-
-        $("#box_field").append(`<div class="box"><p class="box_item">${field[counter].bombs}</p></div>`);
-
-        counter += 1;
-
-      } else {
-        $("#box_field").append(`<div class="box"><p class="box_item">${field[counter].bombs}</p></div>`);
-
-        counter += 1;
-      }
-    }
-  }
-
-  console.log(field);
-
-  let secondCounter = 0;
-  let prova = [];
-
-
-  $("#box_field").css({"width": `${levelChoise(level)[3]}px`, "height": `${levelChoise(level)[4]}px`});
+  fieldBoxes.css({"width": `${levelChoise(level)[3]}px`, "height": `${levelChoise(level)[4]}px`});
 
   $(".box_item").addClass("hide");
 
@@ -113,10 +49,9 @@ $(document).ready(function() {
       if (event.which == 1) {
         $(this).children().removeClass("hide");
       };
-      if (isZero(field[index])) {
-        field[index].opened = true;
-        openAdiacent($(".box"), field, field[index].position[0], field[index].position[1]);
-        console.log(field);
+      if (isZero(newField[index])) {
+        newField[index].isOpened = true;
+        openAdiacent($(".box"), newField, newField[index].position[0], newField[index].position[1]);
         console.log("TRUE");
       } else {
         console.log("FALSE");
@@ -131,10 +66,45 @@ $(document).ready(function() {
 
 });
 
-/// FUNCTIONS
+//----------- FUNCTIONS ------------------------------------>
 
-// vedere se usare includes
-function checkIfDiff(value, array) {
+function numbOfBombs(arraybombs, x, y) {
+  let numOfBombs = 0;
+  const array = [[x-1, y-1],[x-1, y],[x-1, y+1],[x, y-1],[x, y+1],[x+1, y-1],[x+1, y],[x+1, y+1]];
+  for (elem of array) {
+    if (checkBombs(arraybombs, elem[0], elem[1])) {
+      numOfBombs += 1;
+    }
+  }
+  return numOfBombs;
+}
+
+
+function generatefield (level, field, arraybombs, fieldBoxes) {
+  for (let x=1; x<=levelChoise(level)[1][0]; x++) {
+    for (let y=1; y<=levelChoise(level)[1][1]; y++) {
+      console.log(x,y);
+      console.log(field);
+      let id = getId(field, x, y);
+
+      if (!checkBombs(arraybombs, x, y)) {
+
+        let bombs = numbOfBombs(arraybombs, x, y)
+        console.log(field[id]);
+        if (!field[id].isBomb) {
+          field[id].bombs = bombs;
+        }
+        // Metto i numeri nello schema
+        fieldBoxes.append(`<div class="box"><p class="box_item">${field[id].bombs}</p></div>`);
+      } else {
+        // Metto le bombe nello schema
+        fieldBoxes.append(`<div class="box"><p class="box_item">${field[id].bombs}</p></div>`);
+      }
+    }
+  }
+}
+
+function includes(value, array) {
   for (elem of array) {
     if (value.join() == elem.position.join()) {
       return false;
@@ -164,22 +134,70 @@ function levelChoise(value) {
       break;
     case 2:
       gameLevel = randomInteger(0, 479);
-      max = [30,16];
+      max = [16,30];
       bombs = 99;
       width = 1200;
       height = 640;
       break;
   }
-
   return [gameLevel, max, bombs, width, height];
 }
 
-const checkBombs = (objects, i, j) => objects.some(item => item.position.join("") == [i, j].join(""));
+function getId(array, x, y) {
+  let id;
+  console.log(x,y)
+  array.forEach(item => {
+    // console.log(item.position);
+    // console.log([x, y]);
+    if (equalArray(item, x, y)) {
+      id = item.id;
+    }
+  });
+  console.log(id);
+  return id;
+}
+
+function field(level) {
+  let i = 0;
+  let field = []
+  for (let x=1; x<=levelChoise(level)[1][0]; x++) {
+    for (let y=1; y<=levelChoise(level)[1][1]; y++) {
+      let newBox = new Object();
+      newBox.position = [x,y];
+      newBox.id = i;
+      newBox.isOpened = false;
+      newBox.isBomb = false;
+      field.push(newBox);
+      i += 1;
+    }
+  }
+  console.log(field);
+  return field;
+}
+
+function arrayBombs(level, field) {
+  let arraybombs = []
+  while (arraybombs.length < levelChoise(level)[2]) {
+    bomb = levelChoise(level)[0];
+    if (includes(field[bomb].position, arraybombs)) {
+      field[bomb].bombs = "B";
+      field[bomb].isBomb = true;
+      arraybombs.push(field[bomb]);
+    }
+  }
+  return arraybombs;
+}
+
+// rifarla senza some in caso cosi vale per tutti i casi in cui mi serve
+const checkBombs = (array, x, y) => array.some(item => equalArray(item, x, y));
 
 function isZero(object) {
   return object.bombs == 0;
 }
 
+function equalArray(item, x, y) {
+  return (item.position[0] == x && item.position[1] == y)
+}
 
 function openAdiacent(container, array, x, y) {
     let newArr = [];
@@ -225,13 +243,12 @@ function openAdiacent(container, array, x, y) {
         container.eq(elem.id).children().removeClass("hide");
       }
     }
-    console.log(newArr);
+
     for (item of newArr) {
-      if (isZero(item) && item.opened == false) {
-        console.log(item);
-        item.opened = true;
+      if (isZero(item) && item.isOpened == false) {
+        item.isOpened = true;
         openAdiacent(container, array, item.position[0], item.position[1]);
       }
-      item.opened = true;
+      item.isOpened = true;
     }
 }
